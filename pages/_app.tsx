e8a0f8986917e9ff3e187499e6365f59c8e6f26c
Yash Kothari <yashkothari199767@gmail.com>
2024-01-07 21:58:18 +0530

    Adding icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../styles/global.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (<>
+    <Head>
+          <link rel="shortcut icon" href="filecompressor/images/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />;
+    </>)
 }
